b617f4fb2c8b4013cdc055adbb37f9f7bf297c5a
Andres Cabral <cabral.andres@gmail.com>
2025-06-17 09:02:32 -0400

    Added hovering over cards with some subtle effects. There appears to be an issue still with z-index but i'll revisit at some point.

---
+++ card.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+
+func _ready() -> void:
+	# all cards must be a child of cardmanager or this will error
+	get_parent().connect_card_signals(self)
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered", self)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
--- card_manager.gd
+++ card_manager.gd
@@ -2,10 +2,38 @@ extends Node2D
 
 var card_being_dragged
 var screen_size
+var is_hovering_on_card
 const COLLISION_MASK_CARD = 1
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	
+func connect_card_signals(card):
+	card.connect("hovered", on_hovered_over_card)
+	card.connect("hovered_off", on_hovered_off_card)
+
+func on_hovered_over_card(card):
+	if not is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(card, true)
+
+func on_hovered_off_card(card):
+	#is_hovering_on_card = false
+	if !card_being_dragged:
+		highlight_card(card, false)
+		# check if we have hovered off one and onto another
+		var new_card_hovered = raycast_check_for_card()
+		if new_card_hovered:
+			highlight_card(new_card_hovered, true)
+		else:
+			is_hovering_on_card = false
+	
+func highlight_card(card, hovered):
+	if hovered:		
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1,1)
+		card.z_index = 1
 
 func _process(delta: float) -> void:
 	if card_being_dragged:
@@ -19,10 +47,18 @@ func _input(event):
 			#ray cast check for card
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card
+				start_drag(card)
 		else:
-			card_being_dragged = null
-
+			finish_drag()
+			
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(1,1)
+	
+func finish_drag():
+	if (card_being_dragged):
+		card_being_dragged.scale = Vector2(1.05, 1.05)
+		card_being_dragged = null
 
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
@@ -32,6 +68,20 @@ func raycast_check_for_card():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if (result.size() > 0):
-		var thing = result[0].collider
-		return result[0].collider.get_parent()
+		#return result[0].collider.get_parent()
+		return get_card_with_highest_z_index(result)
 	return null
+
+func get_card_with_highest_z_index(cards):
+	# assume the first card in cards array has the highest z index
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	# loop through the rest of the cards checking for a higher z index
+	# skipping the first card here since we checked that already
+	for i in range(1,cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
