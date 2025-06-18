b903751092c22088308a901d42dee38745b9bcd9
Andres Cabral <cabral.andres@gmail.com>
2025-06-17 20:43:41 -0400

    Added a player flipping animation. honestly the animation player bit is very powerful and i should play with that more.

---
+++ Scripts/database.gd
@@ -0,0 +1,5 @@
+const CARDS = { #attack, health
+	"Knight": [2,3],
+	"Archer": [1,1],
+	"Demon": [5,7]
+}
--- Scripts/deck.gd
+++ Scripts/deck.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
-var player_deck = ["knight","knight","knight"]
+var player_deck = ["Knight","Archer","Demon", "Knight"]
+var card_database_reference
 const CARD_SCENE_PATH = "res://Scenes/card.tscn"
 const CARD_DRAW_SPEED = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player_deck.shuffle()
 	$RichTextLabel.text = str(player_deck.size())
+	card_database_reference = preload("res://Scripts/database.gd")
 
 
 func draw_card():
-	var card_drawn = player_deck[0]
-	player_deck.erase(card_drawn)
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
 	
 	if player_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
@@ -21,6 +24,11 @@ func draw_card():
 	$RichTextLabel.text = str(player_deck.size())
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
+	var card_image_path = str("res://Assets/"+ card_drawn_name +".png")
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn_name][1])
+	new_card.get_node("CardImage").texture = load(card_image_path)
 	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
 	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+	new_card.get_node("AnimationPlayer").play("card_flip")
